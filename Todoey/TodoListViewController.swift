5ebb4dd3cd02606ca147b056a2a685692b6ca0bf
Charles Tibbels <chingalator@me.com>
2018-04-08 13:25:44 -0500

    Added user defaults to store list items

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,9 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Read Love Language","Do Taxes","Defeat Zergs"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            
+            itemArray = items
+        }
     }
     //Mark Tableview Data Source Methods
     
@@ -57,6 +63,8 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what happens when user clicks add item
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
        
