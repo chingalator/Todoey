fa420c3b731e34b167dbdef3538dd0cd5e05c54e
Charles Tibbels <chingalator@me.com>
2018-04-08 14:55:47 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Charles Tibbels on 4/8/18.
+//  Copyright © 2018 invescity. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
+
